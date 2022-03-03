f4bc65524c09f4c738dda4d295e8154e9e4b8e90
mismaah <mismaahabdulla@gmail.com>
2022-03-03 12:30:21 +0500

    FEAT: Role guards to cache resolver

--- src/redisCache.resolver.ts
+++ src/redisCache.resolver.ts
@@ -2,17 +2,21 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { RedisCacheService } from './redisCache.service';
 import { GqlAuthGuard } from './guards/gql-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
+import { Roles } from './decorators/roles.decorator';
 
-@UseGuards(GqlAuthGuard)
+@UseGuards(GqlAuthGuard, RolesGuard)
 @Resolver()
 export class RedisCacheResolver {
   constructor(private readonly redisCacheService: RedisCacheService) {}
 
+  @Roles('Admin')
   @Query(() => [String], { name: 'redisKeys' })
   async getKeys(): Promise<string[]> {
     return await this.redisCacheService.getKeys();
   }
 
+  @Roles('Admin')
   @Query(() => String, { name: 'redisGet', nullable: true })
   async redisGet(
     @Args('key', { type: () => String }) key: string
@@ -21,6 +25,7 @@ export class RedisCacheResolver {
     return JSON.stringify(value);
   }
 
+  @Roles('Admin')
   @Mutation(() => String, { name: 'flushRedis' })
   async flushRedis(): Promise<String> {
     await this.redisCacheService.deleteAll();
