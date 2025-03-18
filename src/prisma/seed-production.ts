c5ceddb73ec3c16ee1381269a83a76cd41e589c9
Usagi88 <ibrahimnaish5677@gmail.com>
2025-03-18 21:22:36 +0900

    fix: new user seed

---
+++ src/prisma/seed-production.ts
@@ -0,0 +1,43 @@
+// prisma/seed-production.ts
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const testUserId = 'admin101';
+
+  // Check if user already exists
+  const existingUser = await prisma.user.findUnique({
+    where: { userId: testUserId },
+  });
+
+  if (existingUser) {
+    console.log(`User already exists.`);
+    return;
+  }
+
+  // Create user if they don't exist
+  const hashedPassword = await bcrypt.hash('test', 10);
+
+  await prisma.user.create({
+    data: {
+      userId: 'admin101',
+      email: 'test@gmail.com',
+      password: hashedPassword,
+      fullName: 'Admin User',
+      rcno: 1111,
+    },
+  });
+
+  console.log(`Created a new admin`);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
